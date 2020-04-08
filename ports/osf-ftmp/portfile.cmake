15f160f1ca815f0d87dd3c31741b44d9fb096233
odinthenerd <odinthenerd@gmail.com>
2020-04-09 01:33:24 +0200

    added port for product type

--- ports/osf-ftmp/portfile.cmake
+++ ports/osf-ftmp/portfile.cmake
@@ -1,9 +1,9 @@
 include(vcpkg_common_functions)
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
-    REPO skunkforce/sum_type
-    REF f5fbfaf6d7639aa5840bc263b0ce0e0aa79b0142
-    SHA512 432318df14fafc72d307152846c57b4951f0155f47aae00e91e20f9016bf6e8946f9b29a62a0c96c7edbff1408a46e79607879811bc0d9a7f0665c1d4a614a46
+    REPO skunkforce/ftmp
+    REF e8f08e8265ae908aded6e913a62ed4efa8b8d008
+    SHA512 e8f362be0ce039af402fc3ab1a3f57a033a428e044471af21623299f77381b35cc52a4640b0415c7505a94b58b42fa7242cdb6e9c05293564a9a501ba1f69e43
     HEAD_REF master
 )
 vcpkg_configure_cmake(
@@ -14,16 +14,16 @@ vcpkg_configure_cmake(
 
 vcpkg_install_cmake()
 
-vcpkg_fixup_cmake_targets(CONFIG_PATH lib/cmake/osf-sum-type)
+vcpkg_fixup_cmake_targets(CONFIG_PATH lib/cmake/osf-ftmp)
 file(RENAME ${CURRENT_PACKAGES_DIR}/LICENSE 
-    ${CURRENT_PACKAGES_DIR}/share/osf-sum-type/copyright)
+    ${CURRENT_PACKAGES_DIR}/share/osf-ftmp/copyright)
 file(
     REMOVE_RECURSE
     ${CURRENT_PACKAGES_DIR}/debug/include
     ${CURRENT_PACKAGES_DIR}/debug
     ${CURRENT_PACKAGES_DIR}/lib
     ${CURRENT_PACKAGES_DIR}/debug/lib
-    ${CURRENT_PACKAGES_DIR}/include/osf/sum_type
+    ${CURRENT_PACKAGES_DIR}/include/osf/ftmp
 )
 
 
--- ports/osf-product-type/portfile.cmake
+++ ports/osf-product-type/portfile.cmake
@@ -1,9 +1,9 @@
 include(vcpkg_common_functions)
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
-    REPO skunkforce/sum_type
-    REF f5fbfaf6d7639aa5840bc263b0ce0e0aa79b0142
-    SHA512 432318df14fafc72d307152846c57b4951f0155f47aae00e91e20f9016bf6e8946f9b29a62a0c96c7edbff1408a46e79607879811bc0d9a7f0665c1d4a614a46
+    REPO skunkforce/product_type
+    REF 9885ec075ff1409bf19daafdd26d1aa3956183eb
+    SHA512 a0dfc653b2cdfd737634079df9958ae7a4ab8c0477f4a4dc19ed5ae9915e66dd4ec95464d63f46eac017b96a136f312948897c94d79c21f9686918440bd02b93
     HEAD_REF master
 )
 vcpkg_configure_cmake(
@@ -14,16 +14,16 @@ vcpkg_configure_cmake(
 
 vcpkg_install_cmake()
 
-vcpkg_fixup_cmake_targets(CONFIG_PATH lib/cmake/osf-sum-type)
+vcpkg_fixup_cmake_targets(CONFIG_PATH lib/cmake/osf-product-type)
 file(RENAME ${CURRENT_PACKAGES_DIR}/LICENSE 
-    ${CURRENT_PACKAGES_DIR}/share/osf-sum-type/copyright)
+    ${CURRENT_PACKAGES_DIR}/share/osf-product-type/copyright)
 file(
     REMOVE_RECURSE
     ${CURRENT_PACKAGES_DIR}/debug/include
     ${CURRENT_PACKAGES_DIR}/debug
     ${CURRENT_PACKAGES_DIR}/lib
     ${CURRENT_PACKAGES_DIR}/debug/lib
-    ${CURRENT_PACKAGES_DIR}/include/osf/sum_type
+    ${CURRENT_PACKAGES_DIR}/include/osf/product_type
 )
 
 
